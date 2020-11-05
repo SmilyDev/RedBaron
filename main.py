5c830ae1b9a01cc57a515a8e169ed44348076924
SmilyDev <vasya_yol@mail.ru>
2020-11-05 16:26:53 +0300

    Добился нормального поворота самолета и расчета углов.

--- main.py
+++ main.py
@@ -1,18 +1,79 @@
 import pygame as pg
-import plane as plane
-import math as mt
+import os
+from plane import *
+import math
 
-pg.init()
+debug_text = []
 
-display = pg.display.set_mode()
-display_size = pg.display.get_surface().get_size()
-game_space = {x: 10000, y: 2000}
+def game():
+    pg.init()
+    display = pg.display.set_mode((500, 500))
+    pg.display.set_caption('Красный барон')
+    display_size = pg.display.get_surface().get_size()
+
+    bg = pg.image.load(os.path.join('images', 'BG.jpg'))
+    game_space = {'w': bg.get_width(), 'h': bg.get_height()}
+    mouse_pos = [0, 0]
 
 
-def game:
+    player_pic = [pg.image.load(os.path.join('images', 'RedPlane.png'))]
+    for i in range(len(player_pic)):
+        player_pic[i] = player_pic[i].convert()
+        player_pic[i].set_colorkey((255, 255, 255))
+
+    clock = pg.time.Clock()
+
+    def show_text():
+        global debug_text
+        for i in range(len(debug_text)):
+            f1 = pg.font.Font(None, 20)
+            text_rand = f1.render(debug_text[i], 0, (180, 0, 0))
+            display.blit(text_rand, (10, i*20))
+        debug_text = []
+
+    planes = []
+
+    def draw_window():
+        display.blit(bg, (display_size[0]/2+10 - planes[0].x, display_size[1]/2-planes[0].y))
+
+        for pl in planes:
+            if planes.index(pl) == 0:
+                pl.draw(display, display_size[0]/2-pl.w, display_size[1]/2-pl.h/2)
+
+        show_text()
+        pg.display.update()
 
 
-# Press the green button in the gutter to run the script.
+    run = True
+    while run:
+        #создаем самолет
+        if len(planes) < 1:
+            planes.append(plane(display_size[0]/2+10, display_size[1]/2+10, 3, 0, player_pic[0], 'player'))
+
+        clock.tick(30)
+
+        for pl in planes:
+            pl.move(game_space, display_size)
+
+        mouse_angle, plane_angle, a, b = planes[0].rotation(pg, mouse_pos, display_size)
+
+        debug_text.append('Угол мыши ' + str(round(mouse_angle*180/math.pi)))
+        debug_text.append('Угол самолета ' + str(round(plane_angle*180/math.pi)))
+        debug_text.append('a ' + str(round(a)))
+        debug_text.append('b ' + str(round(b)))
+        debug_text.append('самолет x,y,w,h ' + str(round(planes[0].x)) + '; ' + str(round(planes[0].y)) + '; '+str(planes[0].w) + '; ' +str(planes[0].h))
+
+
+        draw_window()
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+            if event.type == pg.MOUSEMOTION:
+                mouse_pos = event.pos
+
+        debug_text.append('Позиция мыши: ' + str(mouse_pos))
+
 if __name__ == '__main__':
     game()
 
--- plane.py
+++ plane.py
@@ -1,22 +1,74 @@
+import math as mt
+
 class plane:
-    def __init__(self, x, y, w, h, speed, angle, img):
+    def __init__(self, x, y, speed, angle, img, name):
         self.x = x
         self.y = y
-        self.w = w
-        self.h = h
         self.speed = speed
         self.angle = angle
-        self.img = pg.image.load(img)
+        self.img = img
+        self.base_img = img
+        self.w = self.img.get_width()
+        self.h = self.img.get_height()
+        self.name = name
 
-    def show(self, display):
-        # pg.drowrect(display, (0,0,0), self.x, self.y, self.w,  self.h)
-        display.bilt(self.img, (self.x, self.y))
+    def draw(self, display, x, y):
+        display.blit(self.img, (x, y))
 
-    def move():
-        speedX = self.speed * mt.sin(angle)
-        speedY = self.speed * mt.cos(angle)
+    def move(self, game_space, display_size):
+        speedX = self.speed * mt.cos(self.angle)
+        speedY = self.speed * mt.sin(self.angle)
+
+        self.x = display_size[0]/2 if self.x > game_space['w'] -display_size[0]/2 else self.x
+        self.x = game_space['w'] - display_size[0]/2 if self.x < display_size[0]/2 else self.x
+        self.y = display_size[1]/2 if self.y > game_space['h']-display_size[1]/2 else self.y
+        self.y = game_space['h'] -display_size[1]/2 if self.y < display_size[1]/2 else self.y
+
         self.x += speedX
         self.y += speedY
 
-    def rotation():
-        self.img = pg.transform.rotate(self.img, self.angle)
+    def rotation(self, pg, mouse_pos, display_size):
+        b = abs(mouse_pos[0] - 250)
+        a = abs(mouse_pos[1] - 250)
+        mouse_angle = mt.atan(a/b) if b != 0 else 0
+
+        if mouse_pos[0] < display_size[0] / 2:
+            if mouse_pos[1] >= display_size[1] / 2:
+                mouse_angle = mt.pi - mouse_angle
+            else:
+                mouse_angle = mt.pi + mouse_angle
+        elif mouse_pos[1] < display_size[1] / 2:
+            mouse_angle = 2*mt.pi - mouse_angle
+
+        if a == 0:
+            return mouse_angle, self.angle, a, b
+
+        #противоположный самолету угол
+        alt_angle = self.angle + mt.pi
+        if alt_angle > 2*mt.pi:
+            alt_angle -= 2*mt.pi
+
+        #определяем направление поворота самолета
+        if self.angle < mouse_angle - 0.175 or self.angle > mouse_angle + 0.175:
+            if self.angle < mt.pi:
+                if mouse_angle > self.angle and mouse_angle < alt_angle:
+                    self.angle += 0.0175
+                else:
+                    self.angle -= 0.0175
+            else:
+                if mouse_angle < self.angle and mouse_angle > alt_angle:
+                    self.angle -= 0.0175
+                else:
+                    self.angle += 0.0175
+
+
+        if self.angle > 2*mt.pi:
+            self.angle = 0
+        if self.angle < 0:
+            self.angle = 2*mt.pi
+
+        self.img = pg.transform.rotate(self.base_img, -self.angle*180/mt.pi)
+        return mouse_angle, self.angle, a, b
+
+
+
